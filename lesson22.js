c970b8e448f7784761c06f05778f6aa45496870b
khrustalevDev <54330110+khrustalevDev@users.noreply.github.com>
2020-07-13 17:51:52 +0300

    lesson 22 refference

--- lesson22.js
+++ lesson22.js
@@ -12,3 +12,32 @@ clone.b = 20;
 
 console.log('obj: ', obj);
 console.log('clone: ', clone);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+    blogs = ['vk', 'blogger', 'livejournal'],
+    internet = [...video, ...blogs, 'facebook', 'insta'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 3, 4];
+
+log(...num);
+
+const array = ['a', 'b'];
+
+const newArray = [...array];
+
+const test = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...test };
+
+console.log('newObj: ', newObj);
