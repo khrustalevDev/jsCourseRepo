31005c34fffc5a28d431c5a56ab97644c3d125c5
khrustalevDev <54330110+khrustalevDev@users.noreply.github.com>
2020-07-28 16:26:24 +0300

    lesson 41 + 'day cases'

--- lesson38/js/script.js
+++ lesson38/js/script.js
@@ -37,4 +37,77 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    //TIMER
+
+    const promoEndDay = '2020-08-10';
+
+    function getTimeRem(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor((t / (1000 * 60 * 60 * 24))),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24));
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+
+    }
+
+    function addZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function dayCase(days, selector) {
+        const timerBlock = document.querySelector(selector),
+            usedNode = timerBlock.childNodes[2];
+
+        const firstArray = ["0", "5", "6", "7", "8", "9"];
+        const secondArray = ["2", "3", "4"];
+
+        const lastNum = days.charAt(days.length - 1);
+        if (firstArray.includes(lastNum)) {
+            usedNode.nodeValue = 'Дней';
+        } else if (secondArray.includes(lastNum)) {
+            usedNode.nodeValue = 'Дня';
+        } else usedNode.nodeValue = 'День';
+    }
+
+    function setTimer(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = document.querySelector('#days'),
+            hours = document.querySelector('#hours'),
+            minutes = document.querySelector('#minutes'),
+            seconds = document.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+
+        function updateClock() {
+            let t = getTimeRem(endtime);
+            days.innerHTML = addZero(t.days);
+            hours.innerHTML = addZero(t.hours);
+            minutes.innerHTML = addZero(t.minutes);
+            seconds.innerHTML = addZero(t.seconds);
+
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+    }
+
+    setTimer('.timer', promoEndDay);
+    dayCase(getTimeRem(promoEndDay).days.toString(), '.timer__block');
+
 });
