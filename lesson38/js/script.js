554a58ba53bb4af175051de9ca8610c46e3394cd
Err0rmaker <i.khrustalyov@mail.ru>
2020-08-06 23:06:58 +0300

    merge

--- lesson38/js/script.js
+++ lesson38/js/script.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    // TABS
+
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
