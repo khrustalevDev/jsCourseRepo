62b7463b3a8f9ceb7e647e182917f0ed169a3ab5
khrustalevDev <54330110+khrustalevDev@users.noreply.github.com>
2020-08-12 13:17:56 +0300

    lesson 48 done

--- lesson38/js/script.js
+++ lesson38/js/script.js
@@ -210,21 +210,45 @@ window.addEventListener('DOMContentLoaded', () => {
         render() {
             const element = document.createElement('div');
             element.innerHTML = `<div class="menu__item">
-            <img src="img/tabs/vegy.jpg" alt="vegy">
-            <h3 class="menu__item-subtitle">Меню "Фитнес"</h3>
-            <div class="menu__item-descr">Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих
-                овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной
-                ценой
-                и высоким качеством!
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.text}
             </div>
             <div class="menu__item-divider"></div>
             <div class="menu__item-price">
                 <div class="menu__item-cost">Цена:</div>
-                <div class="menu__item-total"><span>229</span> грн/день</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
             </div>
         </div>`;
-
-
+            this.parent.append(element);
         }
     }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        21,
+        ".menu .container"
+    ).render();
 });
