0dfc5148a086242d28fa37dba6a26306844125d4
khrustalevDev <54330110+khrustalevDev@users.noreply.github.com>
2020-09-18 19:21:02 +0300

    added reg expr

--- lesson38/js/script.js
+++ lesson38/js/script.js
@@ -374,10 +374,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
         arrowNext.addEventListener('click', () => {
-            if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+            if (offset == +width.replace(/\D/g, '') * (slides.length - 1)) {
                 offset = 0;
             } else {
-                offset += +width.slice(0, width.length - 2);
+                offset += +width.replace(/\D/g, '');
             }
             slidesField.style.transform = `translateX(-${offset}px)`;
             if (slideIndex == slides.length) {
@@ -398,10 +398,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
         arrowPrev.addEventListener('click', () => {
             if (offset == 0) {
-                offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+                offset = +width.replace(/\D/g, '') * (slides.length - 1);
 
             } else {
-                offset -= +width.slice(0, width.length - 2);
+                offset -= +width.replace(/\D/g, '');
             }
             slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -424,7 +424,7 @@ window.addEventListener('DOMContentLoaded', () => {
             dot.addEventListener('click', (e) => {
                 const slideTo = e.target.getAttribute('data-slide-to');
                 slideIndex = slideTo;
-                offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+                offset = +width.replace(/\D/g, '') * (slideTo - 1);
                 slidesField.style.transform = `translateX(-${offset}px)`;
                 if (slides.length < 10) {
                     currentSlide.textContent = `0${slideIndex}`;
